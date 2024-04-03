4eae27970d7a7fe81c5a8b67ad1e2a80dc122a67
Jiu Kim <jiu.judy@gmail.com>
2024-04-03 17:10:20 -0400

    Practiced some events and network request techniques
    1. used JavaScript to manipulate DOM elements (click, mouse and scroll event)
    2. used different ways to send network requests (jQuery Ajax methods, XMLHttpRequest and fetch methods)

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
 console.log('hello world')
 
-console.log(document)
+const testEl = document.getElementById('test-el')
+
+testEl.textContent = 'bye bye'
+
+testEl.addEventListener('click', ()=>{
+    console.log('clicked')
+    testEl.innerHTML = "<b>clicked</b>"
+})
+
+testEl.addEventListener('mouseover', ()=>{
+    console.log('on')
+})
+
+testEl.addEventListener('mouseout', ()=>{
+    console.log('off')
+})
+
+document.addEventListener('scroll', ()=>{
+    const positionY = window.scrollY
+    console.log(positionY)
+})
+
+
+// GET the data with ajax
+const url = 'https://swapi.dev/api/people/'
+
+// 1. jquery ajax method <- this is what we are going to use in this course
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response){
+        console.log('jquery ajax', response)
+    },
+    error: function(error){
+        console.log(error)
+    }
+})
+// 2. XMLHttpRequest
+
+const req = new XMLHttpRequest()
+
+req.addEventListener('readystatechange', ()=>{
+    if(req.readyState==4){
+        console.log('xhttp', JSON.parse(req.responseText))
+    }
+})
+
+req.open('GET', url)
+req.send()
+
+// 3. fetch method
+
+fetch(url)
+.then(resp=> resp.json()).then(data=> console.log('fetch', data))
+.catch(err=> console.log(err))
+// other popular: ajax library, async await + fetch
