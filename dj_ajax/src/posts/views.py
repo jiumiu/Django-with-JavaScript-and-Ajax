5879dc214cc4eb79d0f2c0eaafac47d5b7e0ea40
Jiu Kim <jiu.judy@gmail.com>
2024-04-05 18:14:04 -0400

    Making able to like and unlike posts through ajax

--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 def post_list_and_create(request):
@@ -26,6 +28,19 @@ def load_post_data_view(request, num_posts):
         }
         data.append(item)
     return JsonResponse({'data':data[lower:upper], 'size': size})
+
+@require_http_methods(["POST"])
+def like_unlike_post(request):
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world x2'})
