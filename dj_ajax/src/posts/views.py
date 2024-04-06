2eb2ef877b8887d45e016278d3db45eec974d2b8
Jiu Kim <jiu.judy@gmail.com>
2024-04-06 00:35:38 -0400

    Created a new view named 'post_detail_data_view'

--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -62,6 +62,16 @@ def load_post_data_view(request, num_posts):
             data.append(item)
         return JsonResponse({'data':data [lower:upper], 'size':size})
 
+def post_detail_data_view(request, pk):
+    obj = Post.objects.get(pk=pk)
+    data ={
+        'id': obj.id,
+        'title': obj.title,
+        'body': obj.body,
+        'author': obj.author.user.username,
+        'logged_in': request.user.username,
+    }
+    return JsonResponse({'data': data})
 
 @require_http_methods(["POST"])
 def like_unlike_post(request):
